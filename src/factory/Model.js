0d00259b5acda78f2421ffb38add7ebf3078f8a2
Joseph Koehler <jkoehler@adelphi.edu>
2021-03-04 09:58:25 -0500

    Update Model.js

--- src/factory/Model.js
+++ src/factory/Model.js
@@ -2,21 +2,41 @@ class Model
 {
 	constructor()
 	{
+		// ImageName -> List of Starfish Model Sprites
+		this.modelGroupSet = {};
 		this.modelSpriteList = [];
 		this.modelInstructionList = [];
 	}
 
-	exportCode()
+	// Old Export Code (Hopefully)
+	/*exportCode()
 	{
 		let code = "";
 
-		for (let modelSprite of this.modelSpriteList) 
+		for (let modelSprite of this.modelSpriteList)
 		{
   			code += modelSprite.exportCode();
 		}
 		return code;
+	}*/
+
+	exportCode()
+	{
+		let code = "";
+		for (let groupName of this.modelGroupSet)
+		{
+			let spriteList = this.modelGroupSet[groupName];
+			code += `let ${groupName}Group = new Group();\n`;
+		}
+	}
+
+	addSpriteToGroup(groupName, spriteToGroup)
+	{
+		if(!(groupName in this.modelGroupSet))
+		{
+			this.modelGroupSet[groupName] = [];
+		}
+
+		this.modelGroupSet[groupName].push(spriteToGroup);
 	}
 }
-
-
-
